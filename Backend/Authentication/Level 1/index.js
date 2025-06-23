d0384a49fc1cde5fc38d836e2bd6f85c6d01cfd1
TanayShah07 <144884960+TanayShah07@users.noreply.github.com>
2025-06-23 23:28:00 +0530

    fourteenth

--- Backend/Authentication/Level 1/index.js
+++ Backend/Authentication/Level 1/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "tanay0709",
+  port: 5432,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,9 +29,48 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const email= req.body.username;
+  const password = req.body.password;
+  try {
+    const checkResult= await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    if (checkResult.rows.length > 0) {
+      res.send("Email already exists. Try logging in.");
+    } else {
+      await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", 
+      [ email, password]
+      );
+      res.render("secrets.ejs");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-app.post("/login", async (req, res) => {});
+app.post("/login", async (req, res) => {
+  const email= req.body.username;
+  const password = req.body.password;
+
+  try {
+    const checkResult= await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    if (checkResult.rows.length > 0) {
+      const user = checkResult.rows[0];
+      if (password === user.password) {
+        res.render("secrets.ejs");
+      } else {
+        res.send("Incorrect password. Please try again.");
+      }
+    } else {
+      res.send ("User not found.");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
